6b859132fd15f4c5a48b3da9c0798e2be5e08d02
Vilma <vilma.l.kangas@helsinki.fi>
2016-02-14 22:33:32 +0200

    nelosviikon palautukset

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,6 +1,6 @@
 class BreweriesController < ApplicationController
   before_action :set_brewery, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate, only: [:destroy]
+  before_action :ensure_that_signed_in, except: [:index, :show]
 
   # GET /breweries
   # GET /breweries.json
@@ -64,14 +64,7 @@ class BreweriesController < ApplicationController
 
   private
 
-  def authenticate
-    admin_accounts = { "admin" => "secret", "pekka" => "beer", "arto" => "foobar", "matti" => "ittam"}
 
-    authenticate_or_request_with_http_basic do |username, password|
-      #username == "admin" and password == "secret"
-      admin_accounts.keys.include? username and admin_accounts[username] = password
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- spec/features/beers_spec.rb
+++ spec/features/beers_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
+include Helpers
+
 describe "Beer" do
+  before :each do
+    FactoryGirl.create :user
+    visit signin_path
+    fill_in('username', with:'Pekka')
+    fill_in('password', with:'Foobar1')
+    click_button('Log in')
+  end
+
   it "can be added when given a valid name" do
-    visit beers_path
-    click_link('New Beer')
+    visit new_beer_path
     fill_in('Name', with:'olut')
     expect{
       click_button('Create Beer')
@@ -11,8 +20,7 @@ describe "Beer" do
   end
 
   it "cannot be added if given name is not valid" do
-    visit beers_path
-    click_link('New Beer')
+    visit new_beer_path
     expect{
       click_button('Create Beer')
     }.to change{Beer.count}.by(0)
